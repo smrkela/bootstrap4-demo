32c35456ea2040f93ea4aeb50de1328581ed971b
Sasa Mrkela <sasa.mrkela@runsimply.com>
2016-12-23 15:53:22 +0100

    Added animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,70 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
 
-        if($(window).scrollTop() > 970){
+        updateNavPosition();
+    })
+
+    function updateNavPosition() {
+
+        if ($(window).scrollTop() > 970) {
 
             $('.navbar').addClass('navbar-fixed-top');
-        } else{
+            // $('body').css('margin-top', '180px');
+        } else {
             $('.navbar').removeClass('navbar-fixed-top');
+            // $('body').css('margin-top', '');
         }
-    })
+    }
+
+    updateNavPosition();
+
+    $("a[href^='#']").on('click', function (event) {
+
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 300, function () {
+            window.location.hash = hash;
+        }
+        );
+    });
+
+    var animatedElementCounter = 0;
+
+    $('.js-wp').each(function (index, item) {
+
+        var $element = $(item);
+        var animationClass = $element.data('animate');
+
+        item.id = "animationId" + ++animatedElementCounter;
+
+        if ($element.data('animate-in') !== undefined)
+            $element.css('opacity', 0);
+
+        if ($element.data('animate-out') !== undefined)
+            $element.css('opacity', 1);
+
+        if ($element.data('animate-delay') !== undefined)
+            $element.css('animation-delay', $element.data('animate-delay'));
+
+        $('#' + item.id).waypoint(function (direction) {
+
+            $item = $(item);
+            $item.addClass('animated ' + animationClass);
+
+            if ($item.data('animate-in') !== undefined)
+                $item.css('opacity', 1);
+
+            if ($item.data('animate-out') !== undefined)
+                $item.css('opacity', 0);
+        }, {
+                offset: '80%'
+            })
+    });
+
 });
+
